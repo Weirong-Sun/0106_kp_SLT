d94a6737b40b155f8012e57ba885c659c506ccd6
Weirong-Sun <swr0311@outlook.com>
2026-01-17 12:03:07 +1300

    test

--- data/extract_phoenix_keypoints_distributed.py
+++ data/extract_phoenix_keypoints_distributed.py
@@ -12,7 +12,7 @@ PHOENIX-2014-T-release-v3/
               ├── dev/
               └── test/
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 data/extract_phoenix_keypoints_distributed.py --dataset_path /data/phd/wsun/datasets/PHOENIX-2014-T-release-v3/PHOENIX-2014-T --output_path phoenix_keypoints.pkl --num_workers 4
+运行脚本：CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 data/extract_phoenix_keypoints_distributed.py --dataset_path /data/phd/wsun/datasets/PHOENIX-2014-T-release-v3/PHOENIX-2014-T --output_path phoenix_keypoints.pkl --num_workers 4
 """
 import os
 import sys
